796ebad14dcce03438338aa909b68fff0756d0a1
Вероника <130035144+doronindm@users.noreply.github.com>
2023-04-12 11:01:08 +0300

    Функция Panic отложенный вызов Defer и восстановление Recover

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import "fmt"
 
-func fib(n uint) uint {
-	if n < 2 {
-		return n
-	}
-
-	return fib(n-1) + fib(n-2)
-}
 func main() {
-	fmt.Println(fib(11))
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	panic("PANIC")
 }
