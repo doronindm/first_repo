23626019574b5a86bff32abcd76966b1addc4cbe
Вероника <130035144+doronindm@users.noreply.github.com>
2023-04-14 12:04:01 +0300

    Комбинированный Интерфейс. Разделение Интерфейсов. Вытаскивание одинаковых методов из разных Интерфейсов для разных структур

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
-type animal interface {
+type fish interface {
 	swimming
+	diving
+	flight
+}
+type bird interface {
+	walker
 	diving
 	flight
 }
@@ -16,7 +21,11 @@ type diving interface {
 type flight interface {
 	fly()
 }
+type walker interface {
+	walk()
+}
 type flyingFish struct{}
+type seagull struct{}
 
 func (f *flyingFish) swim() {
 	fmt.Println("Flying fish swim")
@@ -27,9 +36,21 @@ func (f *flyingFish) dive() {
 func (f *flyingFish) fly() {
 	fmt.Println("Flying fish fly")
 }
+func (s *seagull) walk() {
+	fmt.Println("Seagull walk")
+}
+func (s *seagull) dive() {
+	fmt.Println("Seagull dive")
+}
+func (s *seagull) fly() {
+	fmt.Println("Seagull fly")
+}
+func dive(d diving) {
+	d.dive()
+}
 func main() {
-	var f animal = &flyingFish{}
-	f.swim()
-	f.dive()
-	f.fly()
+	var f fish = &flyingFish{}
+	var s bird = &seagull{}
+	dive(f)
+	dive(s)
 }
